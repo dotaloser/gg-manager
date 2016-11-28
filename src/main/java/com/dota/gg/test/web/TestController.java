7c464aed985f9b02f140eae86df6238743fd34da
dennis <waywaitway@163.com>
2016-11-28 11:01:28 +0800

    WebAppRootListener test

--- src/main/java/com/dota/gg/test/web/TestController.java
+++ src/main/java/com/dota/gg/test/web/TestController.java
@@ -1,14 +1,18 @@
 package com.dota.gg.test.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/test")
 public class TestController {
-	
+	private Logger logger = LoggerFactory.getLogger(TestController.class);
 	@RequestMapping(value = "/a")
 	public String getTestPage(){
+		logger.info("gm.root:"+System.getProperty("gm.root"));
+		logger.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 		return "/test/test";
 	}
 }
